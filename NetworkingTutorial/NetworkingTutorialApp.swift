8b0c23112e2ec28dc86cb3d82ec561ac91eb69e1
Onlyllon1 <francescoliviero15@gmail.com>
2024-03-15 16:30:52 +0100

    Initial Commit

---
+++ NetworkingTutorial/NetworkingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTutorialApp.swift
+//  NetworkingTutorial
+//
+//  Created by Francesco on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
