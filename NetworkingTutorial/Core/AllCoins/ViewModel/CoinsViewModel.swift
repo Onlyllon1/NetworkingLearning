30a2e18ff5eb7b53484a200f163d549c6871ee13
Onlyllon1 <francescoliviero15@gmail.com>
2024-03-17 19:48:55 +0100

    App based on learning networking

--- NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+class CoinsViewModel: ObservableObject {
+    
+    @Published var coin = " "
+    @Published var price = " "
+    
+    init(){
+        fetchPrice(coin: "ethereum")
+    }
+    
+    func fetchPrice(coin: String) {
+//       salvo l' url in una variabile
+        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=eur"
+//        converto la variabile nel tipo URL
+        guard let url = URL(string: urlString) else {return}
+        
+        
+        print("Fetching price..")
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            print("Did receive data \(data)")
+            guard let data = data else { return }
+            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+            print(jsonObject)
+            guard let value = jsonObject[coin] as? [String: Double] else {
+                print ("Failed to parse value")
+                return
+            }
+            guard let price = value["eur"] else { return }
+            print(value)
+            
+            DispatchQueue.main.async {
+                self.coin = coin.capitalized
+                self.price = "$\(price)"
+            }
+ 
+        }.resume()
+        
+        print("Did reach end of function")
+    }
+}
